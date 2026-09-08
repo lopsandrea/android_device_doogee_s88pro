e37049eb32dcbdb3bbebb6ff6f0145b613c2b282
lopsandrea <h2o.al.97@gmail.com>
2026-09-08 17:34:04 +0200

    via PRODUCT_BUILD_GENERIC_OTA_PACKAGE: vale solo per i device A/B
    Rompeva perfino lunch:
    
        board_config.mk:982: error: PRODUCT_BUILD_GENERIC_OTA_PACKAGE with
        'AB_OTA_UPDATER != true' is not supported.
    
    Questo device e non-A/B. Lo zip installabile su non-A/B richiede il recovery,
    perche e il recovery ad applicare l aggiornamento: senza, AOSP non calcola
    recovery_fstab e spegne build_ota_package. Non e un capriccio del build.
    
    Finche si usa la TWRP di lopestom e non si costruisce il recovery di
    LineageOS, le immagini si producono e si installano singolarmente.

--- lineage_s88pro.mk
+++ lineage_s88pro.mk
@@ -9,15 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/doogee/s88pro/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Fa uscire la ROM installabile senza costruire il recovery: il BoardConfig ha
-# TARGET_NO_RECOVERY := true (si usa la TWRP di lopestom, e costruirlo fallisce
-# comunque su mkbootimg), ma il build lega le due cose e senza questa riga
-# INTERNAL_OTA_PACKAGE_TARGET resta vuota -- il target "bacon" finisce per
-# eseguire "ln -f" su uno zip che nessuno ha costruito.
-#
-# Sta QUI e non in device.mk: da li' non veniva raccolta.
-PRODUCT_BUILD_GENERIC_OTA_PACKAGE := true
-
 PRODUCT_DEVICE := s88pro
 PRODUCT_NAME := lineage_s88pro
 PRODUCT_BRAND := DOOGEE
